f341cc6a269b4f940d3771b853c99a8d616e479f
koratkar <woohooyeshaan@gmail.com>
2020-05-17 20:01:57 -0500

    cool new settings view

---
+++ WaterScape/Controllers/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  WaterScape
+//
+//  Created by Ishaan Koratkar on 5/17/20.
+//  Copyright © 2020 Ishaan Koratkar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

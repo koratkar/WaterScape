329c4c84bfd1abc84d6a39ea7fd903d70d2e89c5
koratkar <woohooyeshaan@gmail.com>
2020-06-05 09:58:02 -0500

    Implemented User Defaults

--- WaterScape/Controllers/SettingsViewController.swift
+++ WaterScape/Controllers/SettingsViewController.swift
@@ -8,31 +8,73 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
 
+class SettingsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        let savedRepeat = defaults.integer(forKey: "RepeatNumber")
+        if savedRepeat == -1 {
+            repeatSwitch.setOn(true, animated: false)
+        } else {
+            repeatSwitch.setOn(false, animated: false)
+        }
+       
     }
+    
+    
+    
+    let defaults = UserDefaults.standard
+    
+    
+    
     
     var VCController = ViewController()
     var repeatLabel: UILabel?
+    var repeatNumber: Int?
+    var delegate: audioControl?
+    var playerIsPlaying: Bool?
+    var chosenSound = "repeatTester.wav"
+    
+
+    
+    
     
     @IBOutlet weak var repeatSwitch: UISwitch!
     
     
     @IBAction func repeatSwitchPressed(_ sender: Any) {
         if repeatSwitch.isOn == true {
-            VCController.repeatForever = true
+            //repeatNumber and text are changed
+            repeatNumber = -1
+            self.delegate?.repeatNumber = -1
             repeatLabel?.text = "on"
+            defaults.set(repeatNumber, forKey: "RepeatNumber")
+            
+            if playerIsPlaying == true {
+                self.delegate?.setUpSound(soundName: chosenSound, numberOfRepeats: repeatNumber!)
+                self.delegate?.playSound()
+            } else {
+                self.delegate?.setUpSound(soundName: chosenSound, numberOfRepeats: repeatNumber!)
+            }
+            
+            
+            
         } else if repeatSwitch.isOn != true {
-            VCController.repeatForever = false
-            repeatLabel?.text = "off"
+            repeatNumber = 0
+            self.delegate?.repeatNumber = 0
+            defaults.set(repeatNumber, forKey: "RepeatNumber")
+            
+            if playerIsPlaying == true {
+                self.delegate?.setUpSound(soundName: chosenSound, numberOfRepeats: repeatNumber!)
+                self.delegate?.playSound()
+            } else {
+                self.delegate?.setUpSound(soundName: chosenSound, numberOfRepeats: repeatNumber!)
+            }
+            
+            
         }
-    }
-    
-    
+}
    
     /*
     // MARK: - Navigation
@@ -43,5 +85,6 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
 
 }
--- WaterScape/Controllers/ViewController.swift
+++ WaterScape/Controllers/ViewController.swift
@@ -9,36 +9,47 @@
 import UIKit
 import AVFoundation
 import AVKit
-import MediaPlayer
 
+protocol audioControl {
+    func setUpSound(soundName: String, numberOfRepeats: Int)
+    func playSound()
+    var repeatNumber: Int { get set }
+}
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, audioControl {
+    
     
     var player: AVAudioPlayer!
+    var settingsViewController: SettingsViewController!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setUpSound(soundName: "rainsound.wav")
+        
+        //when you create a default without a value, it's default value is 0, but I can register a "default for my default"
+        defaults.register(defaults: ["RepeatNumber": -1])
+        let savedRepeat = defaults.integer(forKey: "RepeatNumber")
+        
+        setUpSound(soundName: "repeatTester.wav", numberOfRepeats: savedRepeat)
     }
     
-    var deleteThis = "a variable"
-    var isOn: Bool = false
-    var repeatForever = false
+    let defaults = UserDefaults.standard
+    var repeatNumber = -1
     
     let playing = UIImage(systemName: "bolt.fill")
     let paused = UIImage(systemName: "bolt")
-
     
-    func setUpSound(soundName: String) {
+    
+    func setUpSound(soundName: String, numberOfRepeats: Int) {
         let path = Bundle.main.path(forResource: soundName, ofType: nil)!
         let url = URL(fileURLWithPath: path)
         do {
             player = try AVAudioPlayer(contentsOf: url)
+            player.numberOfLoops = numberOfRepeats
         } catch {
             print("didn't work bro")
-            isOn = false
+            
         }
         
         let session = AVAudioSession.sharedInstance()
@@ -46,19 +57,19 @@ class ViewController: UIViewController {
         do {
             try session.setCategory(AVAudioSession.Category.playback)
         } catch {
-            print("something went wrong, chopper") 
-        } 
+            print("something went wrong, chopper")
+        }
+        
     }
     
     func playSound() {
         player.play()
-        isOn = true
         playButton.setImage(playing, for: .normal)
     }
     
+    
     func stopSound() {
-        player!.pause()
-        isOn = false
+        player.pause()
         playButton.setImage(paused, for: .normal)
     }
     
@@ -68,14 +79,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     
     @IBAction func playButton(_ sender: Any) {
-        if isOn == false {
+        if player.isPlaying == false {
             playSound()
         } else {
             stopSound()
         }
+        
+        //if content stops playbutton = offState thing added
     }
     
-    
+    //dead repeat forever label, add a real one
     @IBOutlet var repeatLabel: UILabel!
     
     
@@ -88,9 +101,18 @@ class ViewController: UIViewController {
         if segue.identifier == "goToSettings" {
             // Get the new view controller using segue.destination.
             let repeatLabel = self.repeatLabel
+            let repeatNumber = self.repeatNumber
+            
                // Pass the selected object to the new view controller.
             let settingsViewController = segue.destination as! SettingsViewController
+            settingsViewController.delegate = self
             settingsViewController.repeatLabel = repeatLabel
+            settingsViewController.repeatNumber = repeatNumber
+            settingsViewController.playerIsPlaying = player.isPlaying
+            
+            //delete me
+            
+          
         }
         
     }
